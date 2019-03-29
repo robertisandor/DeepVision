0557e8a64947dba056dffde8f00ceaf8798135ff
Robert Sandor <robert.i.sandor@gmail.com>
2019-03-29 09:51:27 -0700

    conforms to pycodestyle

--- group_hw_1/code/user_definition.py
+++ group_hw_1/code/user_definition.py
@@ -1,20 +1,21 @@
 from getpass import getpass
 
 
-ec2_address = "ec2-35-160-123-177.us-west-2.compute.amazonaws.com"
+ec2_address = "ec2-3-80-157-90.compute-1.amazonaws.com"
 
-key_file = "/.AWSkp/r0mer0mOregon.pem"
+key_file = "/Documents/Keys/msds697.pem"
 
-apikey = "AIzaSyBRy6kfXZwbSLc0YZUfiG5IZmwWfAUjglU-E"
+apikey = "AIzaSyCY-x0Jk5rsVnP8w-Ozn3YwzUmK6nUcvHU"
 
 user = "ec2-user"
 
 git_repo_name = "product-analytics-group-project-deepvision"
-git_user_id = "MSDS698" # repo creator
+git_user_id = "MSDS698"  # repo creator
 
 orig_coord = '37.7909,-122.3925'
 dest_coord = '37.7765,-122.4506'
 output_file_name = 'output.txt'
+
 
 def git_credentials():
     git_user = input('Input your git user:\n')
