d738e2c5a13cc610285c4817e5cf012cc1cf686b
r0mer0m <mromerocalvo@dons.usfca.edu>
2019-04-08 20:56:16 -0700

    Cleanin user def

--- code/user_definition.py
+++ code/user_definition.py
@@ -1,11 +1,9 @@
 from getpass import getpass
 
 
-ec2_address = "ec2-35-162-133-15.us-west-2.compute.amazonaws.com"
+ec2_address = "ec2-34-217-134-182.us-west-2.compute.amazonaws.com"
 
 key_file = "/.AWSkp/r0mer0mOregon.pem"
-
-apikey = "AIzaSyCY-x0Jk5rsVnP8w-Ozn3YwzUmK6nUcvHU"
 
 user = "ec2-user"
 
@@ -15,5 +13,4 @@ git_user_id = "MSDS698"  # repo creator
 
 def git_credentials():
     git_user = input('Input your git user:\n')
-    # git_password = getpass('Input your git password:\n')
-    return git_user#, git_password
+    return git_user
