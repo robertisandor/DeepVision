3d577a5e1a63f9d8a6f1b0f63375ae49dc160cf0
HaiVuLe <hailevu.vn@gmail.com>
2019-04-09 19:49:32 -0700

    add a line for testing on local machine

--- code/config.py
+++ code/config.py
@@ -4,5 +4,6 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 # config DB
 ## this is to be changed to make connect with RDB
 class Config(object):
-    SQLALCHEMY_DATABASE_URI = 'postgresql://msds603master:msds603master@msds603test.cpk2y02osnmj.us-east-2.rds.amazonaws.com:5432/test'
+    # SQLALCHEMY_DATABASE_URI = 'postgresql://msds603master:msds603master@msds603test.cpk2y02osnmj.us-east-2.rds.amazonaws.com:5432/test'
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'week3.db') # for testing on local machine
     SQLALCHEMY_TRACK_MODIFICATIONS = True
