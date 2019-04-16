a8f50d50984539aa5e1117aa1b166697c9c75e3c
HaiVuLe <hailevu.vn@gmail.com>
2019-04-15 18:30:55 -0700

    fix link to RBS

--- code/config.py
+++ code/config.py
@@ -7,10 +7,9 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 # this is to be changed to make connect with RDB
 class Config(object):
     # server
-    # SQLALCHEMY_DATABASE_URI = 'postgresql://msds603master:msds603master\
-    # @msds603test.cpk2y02osnmj.us-east-2.rds.amazonaws.com:5432/test'
+    SQLALCHEMY_DATABASE_URI = 'postgresql://msds603master:msds603master@msds603test.cpk2y02osnmj.us-east-2.rds.amazonaws.com:5432/test'
 
     # for testing on local machine
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + \
-                              os.path.join(basedir, 'deepvision.db')
+    # SQLALCHEMY_DATABASE_URI = 'sqlite:///' + \
+    #                           os.path.join(basedir, 'deepvision.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = True
