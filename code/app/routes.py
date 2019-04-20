8b41a6c8001c6abce3310935a14a2e73a49274d8
r0mer0m <mromerocalvo@dons.usfca.edu>
2019-04-19 23:27:53 -0700

    Modify for string reader

--- code/app/routes.py
+++ code/app/routes.py
@@ -10,6 +10,10 @@ from flask import (render_template,
 
 import json
 
+from flask_wtf.file import FileField, FileRequired
+from wtforms import SubmitField
+from werkzeug import secure_filename
+
 # for building forms
 from flask_wtf import FlaskForm  # RecaptchaField
 from wtforms import SubmitField
@@ -166,6 +170,40 @@ def projects():
 			return render_template('projects.html', 
 									projects=[proj[0].strip(",") for proj in projects])
 
+class UploadFileForm(FlaskForm):
+	"""Class for uploading file when submitted"""
+	file_selector = FileField('File')#, validators=[FileRequired()])
+	submit = SubmitField('Submit')
+
+
+@application.route('/project/<projid>', methods=['GET', 'POST'])
+@login_required
+def project(projid):
+	labels = classes.Label.query.filter_by(project_id=projid).all()
+	projnm = classes.User_Project.query.filter_by(project_id=projid).first().project_name
+
+	file = UploadFileForm()  # file : UploadFileForm class instance
+	if file.validate_on_submit():  # Check if it is a POST request and if it is valid.
+		f = file.file_selector.data  # f : Data of FileField
+		filename = secure_filename(f.filename)
+		# filename : filename of FileField
+		# secure_filename secures a filename before storing it directly on the filesystem.
+		file_content = f.stream.read()
+
+		bucket_name = 'msds603-deep-vision'  # Change it to your bucket.
+		###
+		s3_connection = boto.connect_s3(aws_access_key_id='AKIAIQRI4EE5ENXNW6LQ', \
+										aws_secret_access_key='2gduLL4umVC9j7XXc2L1N8DfUVQQKcFmnezTYF8O')
+		bucket = s3_connection.get_bucket(bucket_name)
+		k = Key(bucket)
+		k.key = projid + '/' + filename
+		k.set_contents_from_string(file_content)
+
+		return redirect(url_for('project', projid=projid))
+
+	return render_template('project_d.html', projnm=projnm, labels=labels, form=file)
+
+
 
 @application.route('/logout')
 @login_required
@@ -201,10 +239,10 @@ def mobile_register():
 			db.session.add(user)
 			db.session.commit()
 			# return "1" #
-			return jsonify(success=1)
+			return json.dumps(jsonify(success=1))
 
 	# return "0"
-	return jsonify(success=0)
+	return json.dumps(jsonify(success=0))
 
 
 @application.route('/mobile_signin', methods=['GET', 'POST'])
@@ -245,7 +283,7 @@ def mobile_projects():
 		# return render_template('projects.html', projects=list(projects))
 		projects = [proj[0].strip(",") for proj in projects]
 		# return json.dumps(projects)
-		return jsonify(success=1, projects=json.dumps(projects))
+		return json.dumps(jsonify(success=1, projects=json.dumps(projects)))
 
 	elif request.method == 'POST':
 
@@ -259,7 +297,7 @@ def mobile_projects():
 
 		if len(projects_with_same_name) > 0:
 			# return json.dumps([])
-			return jsonify(success=0)
+			return json.dumps(jsonify(success=0))
 		else:
 			# insert into the Project table
 			db.session.add(classes.Project(project_name, int(current_user.id)))
@@ -297,48 +335,9 @@ def mobile_projects():
 			projects = [proj[0].strip(",") for proj in projects]
 
 			# return  json.dumps(projects)
-			return jsonify(success=1, projects=json.dumps(projects))
+			return json.dumps(jsonify(success=1, projects=json.dumps(projects)))
 			# return render_template('projects.html',
 			# 					   projects=[proj[0].strip(",") for proj in projects])
-
-
-from flask_wtf.file import FileField, FileRequired
-from wtforms import SubmitField
-from werkzeug import secure_filename
-
-
-class UploadFileForm(FlaskForm):
-    """Class for uploading file when submitted"""
-    file_selector = FileField('File')#, validators=[FileRequired()])
-    submit = SubmitField('Submit')
-
-
-@application.route('/project/<projid>', methods=['GET', 'POST'])
-@login_required
-def project(projid):
-	labels = classes.Label.query.filter_by(project_id=projid).all()
-	projnm = classes.User_Project.query.filter_by(project_id=projid).first().project_name
-
-	file = UploadFileForm()  # file : UploadFileForm class instance
-	if file.validate_on_submit():  # Check if it is a POST request and if it is valid.
-		f = file.file_selector.data  # f : Data of FileField
-		filename = secure_filename(f.filename)
-		# filename : filename of FileField
-		# secure_filename secures a filename before storing it directly on the filesystem.
-		file_content = f.stream.read()
-
-		bucket_name = 'msds603-deep-vision'  # Change it to your bucket.
-		###
-		s3_connection = boto.connect_s3(aws_access_key_id='AKIAIQRI4EE5ENXNW6LQ', \
-										aws_secret_access_key='2gduLL4umVC9j7XXc2L1N8DfUVQQKcFmnezTYF8O')
-		bucket = s3_connection.get_bucket(bucket_name)
-		k = Key(bucket)
-		k.key = projid + '/' + filename
-		k.set_contents_from_string(file_content)
-
-		return redirect(url_for('project', projid=projid))
-
-	return render_template('project_d.html', projnm=projnm, labels=labels, form=file)
 
 
 @application.route('/mobile_logout')
@@ -347,5 +346,4 @@ def mobile_logout():
 	logout_user()
 	# flash('You have been logged out.')
 	# return "1"	
-	return jsonify(success=1)
-
+	return json.dumps(jsonify(success=1))
