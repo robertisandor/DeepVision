a82ebdf619fd3c7b2caf09e050cba117508e6800
robertisandor <risandor@usfca.edu>
2019-05-07 16:04:12 +0000

    changed EC2 address

--- code/user_definition.py
+++ code/user_definition.py
@@ -1,6 +1,6 @@
 
 # ec2_address = "ec2-54-191-79-88.us-west-2.compute.amazonaws.com"
-ec2_address = "ec2-100-24-66-125.compute-1.amazonaws.com"
+ec2_address = "ec2-3-212-208-200.compute-1.amazonaws.com"
 
 # key_file = "./deepvision.pem"
 key_file = "./deepVision.pem"
